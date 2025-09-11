590ca21e926efe845782a9c339e4427527dd33f2
biplawofficial <biplawofficial@gmail.com>
2025-09-11 23:25:03 +0530

    pushing final code 4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({ 
-  origin: "http://localhost:3000", 
+  origin: 'https://oralvis-frontend-one.vercel.app', 
   credentials: true 
 }));
 app.use(express.json());
